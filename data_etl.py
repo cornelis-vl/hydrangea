6e1f7fc079d4f68ab6f3d6b126ed1d6b2cc71e22
cornelis-vl <cornelisvletter@gmail.com>
2017-06-23 16:17:22 +0200

    Finished ETL.

--- data_etl.py
+++ data_etl.py
@@ -3,7 +3,13 @@ import cv2
 import time
 import glob
 import pandas as pd
+import numpy as np
 from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping
+from keras.utils import np_utils
 
 HOME_DIR = '/Users/cornelisvletter/Desktop/Programming/Personal'
 PROJECT_FOLDER = '/Kaggle/hydrangea'
@@ -18,16 +24,18 @@ os.chdir(HOME_DIR + PROJECT_FOLDER + DATA)
 
 def get_im_cv2(path):
     img = cv2.imread(path)
-    #resized = cv2.resize(img, (32, 32), cv2.INTER_LINEAR)
-    return img
+    resized_img = cv2.resize(img, (32, 32), cv2.INTER_LINEAR)
+    return resized_img
+
 
 def add_labels(dir, ids):
     label_file = glob.glob1(dir, '*.csv')[0]
-    labels = pd.read_csv(label_file, sep=',')
+    file_path = dir + '/' + label_file
+    labels = pd.read_csv(file_path, sep=',')
     id_df = pd.DataFrame(ids)
     id_df.columns = ['id']
 
-    y_train = id_df.merge(
+    target = id_df.merge(
         labels,
         how='left',
         left_on='id',
@@ -35,11 +43,14 @@ def add_labels(dir, ids):
         indicator=True,
         copy=True)
 
-    return y_train
+    y_train = target['invasive']
+    x_train_id = target['id']
+
+    return y_train, x_train_id
 
 
 def load_images(folder, subset=100):
-    img_array = []
+    x_train = []
     img_ids = []
 
     start_time = time.time()
@@ -51,19 +62,70 @@ def load_images(folder, subset=100):
 
     for fl in files[:subset_size]:
         flbase = int(os.path.basename(fl)[:-4])
-        img = cv2.imread(fl)
-        img_array.append(img)
+        img_path = folder + '/' + fl
+        img = get_im_cv2(img_path)
+        x_train.append(img)
         img_ids.append(flbase)
         if len(img_ids) % 100 == 0:
             print('Finished reading image {}.'.format(len(img_ids)))
 
     print('Read train data time: {} seconds'.format(round(time.time() - start_time, 2)))
 
-    return img_array, img_ids
+    y_train, x_train_id = add_labels(dir=folder, ids=img_ids)
+
+    return x_train, x_train_id, y_train
 
 
 train_dir = HOME_DIR + PROJECT_FOLDER + DATA + '/train'
 test_dir = HOME_DIR + PROJECT_FOLDER + DATA + '/test'
 
-tr_snip, trid_snip = load_images(train_dir, subset=5)
+x5_train, x5_train_id, y5_train = load_images(folder=train_dir, subset=5)
+
+X_train = x5_train
+X_train_id = x5_train_id
+Y_train = y5_train
+
+def etl():
+    X_train, X_train_id, Y_train = load_images(folder=train_dir, subset=5)
+
+    X_train = np.array(X_train, dtype=np.uint8)
+    X_train = X_train.astype('float32')
+    X_train = X_train / 255
+
+    Y_train = np.array(Y_train, dtype=np.uint8)
+    Y_train = np_utils.to_categorical(Y_train, 2)
+
+    X_train_id  = np.array(X_train_id, dtype=np.uint8)
+
+    return X_train, X_train_id, Y_train
+
+X_train, X_train_id, Y_train = etl()
+
+def create_model():
+    model = Sequential()
+    model.add(ZeroPadding2D((1, 1), input_shape=(3, 32, 32), dim_ordering='th'))
+    model.add(Convolution2D(8, 3, 3, activation='relu', dim_ordering='th', init= 'lecun_uniform', subsample=(1,1)))
+    model.add(ZeroPadding2D((1, 1), dim_ordering='th'))
+    model.add(Convolution2D(8, 3, 3, activation='relu', dim_ordering='th', init= 'lecun_uniform', subsample=(1,1)))
+    model.add(Dropout(0.2))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), dim_ordering='th'))
+
+    model.add(ZeroPadding2D((1, 1), dim_ordering='th'))
+    model.add(Convolution2D(16, 3, 3, activation='relu', dim_ordering='th', init= 'lecun_uniform', subsample=(1,1)))
+    model.add(ZeroPadding2D((1, 1), dim_ordering='th'))
+    model.add(Convolution2D(16, 3, 3, activation='relu', dim_ordering='th', init= 'lecun_uniform', subsample=(1,1)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), dim_ordering='th'))
+    model.add(Dropout(0.2))
+
+    model.add(Flatten())
+    model.add(Dense(32, activation='relu', init= 'lecun_uniform'))
+    model.add(Dropout(0.4))
+    model.add(Dense(32, activation='relu', init= 'lecun_uniform'))
+    model.add(Dropout(0.2))
+    model.add(Dense(8, activation='softmax'))
+
+    sgd = SGD(lr=1e-2, decay=1e-4, momentum=0.9, nesterov=False)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy')
+
+    return model
 
