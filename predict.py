2317729a8aa64cea638f3b0ad97c5ee8e3ccef60
cornelis-vl <cornelisvletter@gmail.com>
2017-06-27 11:33:52 +0200

    updated script

--- predict.py
+++ predict.py
@@ -16,14 +16,19 @@ from keras.utils import np_utils
 from matplotlib import pyplot as plt
 from sklearn.metrics import classification_report
 
+# Ignore Keras updates
+import warnings
+warnings.filterwarnings("ignore")
+
+
 # Functions
 
 # I/O, ETL and other process functions.
 
-def get_im_cv2(path, resize_dim=(32,32)):
+def get_im_cv2(path, resize_to=(32,32)):
     """Get image using the image path and resize to (32 x 32) by default."""
     input_img = cv2.imread(path)
-    resized_img = cv2.resize(input_img, resize_dim, cv2.INTER_LINEAR)
+    resized_img = cv2.resize(input_img, resize_to, cv2.INTER_LINEAR)
 
     return resized_img
 
@@ -49,7 +54,7 @@ def add_labels(dir, ids):
     return target, target_id
 
 
-def image_loader(folder, subset=100):
+def image_loader(folder, subset=100, resize_dim=(32,32), add_target=True):
     """Loads all images to array.
     Selects all available images unless subset is set to value between 1-99.
     """
@@ -73,7 +78,7 @@ def image_loader(folder, subset=100):
 
         file_base = int(os.path.basename(file)[:-4])
         img_path = folder + '/' + file
-        img = get_im_cv2(img_path)
+        img = get_im_cv2(img_path, resize_to=resize_dim)
         imgs_collector.append(img)
         img_ids.append(file_base)
         process_no += 1
@@ -84,13 +89,19 @@ def image_loader(folder, subset=100):
     print('Total process time: {} seconds'.format(round(time.time() - start_time, 2)))
 
     # Create target variable for the selected images in the correct order.
-    target, target_ids = add_labels(dir=folder, ids=img_ids)
+    if add_target:
+        target, target_ids = add_labels(dir=folder, ids=img_ids)
+
+    elif not add_target:
+        target = None
+        target_ids = img_ids
 
     return imgs_collector, target_ids, target
 
 
-def etl_images(image_loc, subset=100):
-    features_raw, ids, target_raw = image_loader(folder=image_loc, subset=subset)
+def etl_images(image_loc, subset=100, resize_to=(32,32), train=True):
+    features_raw, ids, target_raw = image_loader(folder=image_loc, subset=subset,
+                                                 resize_dim=resize_to, add_target=train)
 
     # To array, transpose to fit model, and standardized for max. pixel value (255).
     X_train = np.array(features_raw, dtype=np.uint8)
@@ -99,8 +110,12 @@ def etl_images(image_loc, subset=100):
     X_train = X_train / 255
 
     # To array and categorical (one column for each class, 2 columns here)
-    Y_train = np.array(target_raw, dtype=np.uint8)
-    Y_train = np_utils.to_categorical(Y_train, 2)
+
+    if train:
+        Y_train = np.array(target_raw, dtype=np.uint8)
+        Y_train = np_utils.to_categorical(Y_train, 2)
+    elif not train:
+        Y_train = None
 
     X_train_id  = np.array(ids, dtype=np.uint8)
 
@@ -146,14 +161,18 @@ def train_test_split(features, target, identifier, prop=0.7):
     return train_features, test_features, train_target, test_target, train_id, test_id
 
 
-def cov_neural_net():
+def cov_neural_net(input, output):
     """Store all model settings in one function"""
+
+    input_layer_shape = input.shape[1:4]
+    output_layer_shape = output.shape[1]
+
 
     # Convolution part
     model = Sequential()
 
     # Zero padding adds a row of zeros to top/bottom and column of zeros to left/right of features
-    model.add(ZeroPadding2D((1, 1), input_shape=(3, 32, 32), dim_ordering='th'))
+    model.add(ZeroPadding2D((1, 1), input_shape=input_layer_shape, dim_ordering='th'))
 
     # 1-st convolution: This convolution returns an output of (8x8) using a (3x3) filter
     model.add(Convolution2D(8, 3, 3, activation='relu', dim_ordering='th', init= 'lecun_uniform', subsample=(1,1)))
@@ -188,7 +207,7 @@ def cov_neural_net():
     model.add(Dropout(0.2))
 
     # Output for 2 classes
-    model.add(Dense(2, activation='softmax'))
+    model.add(Dense(output_layer_shape, activation='softmax'))
 
     # Optimizer (stochastic gradient descent)
     sgd = SGD(lr=1e-2, decay=1e-4, momentum=0.9, nesterov=False)
@@ -200,7 +219,7 @@ def cov_neural_net():
 
 if __name__ == '__main__':
 
-    PROJECT_DIR = os.getcwd() # locate to the 'hydrangea' folder
+    PROJECT_DIR = os.getcwd()  # locate to the 'hydrangea' folder
     DATA = '/data'
 
     train_dir = PROJECT_DIR + DATA + '/train'
@@ -214,9 +233,14 @@ if __name__ == '__main__':
         train_dir = PROJECT_DIR + DATA + '/train'
     elif folder_input == 'preview':
         train_dir = PROJECT_DIR + DATA + '/train_preview'
+    elif folder_input == 'submission':
+        train_dir = PROJECT_DIR + DATA + '/train'
+        test_dir = PROJECT_DIR + DATA + '/test'
 
 
-    X_train, X_train_id, Y_train = etl_images(image_loc=train_dir, subset=subset_input)
+
+    X_train, X_train_id, Y_train = etl_images(image_loc=train_dir, subset=subset_input,
+                                              resize_to=(128,128), train=True)
 
     # Split to train and test datasets
     X_train_train, X_train_test, Y_train_train,\
@@ -224,7 +248,8 @@ if __name__ == '__main__':
                                                                    target=Y_train,
                                                                    identifier=X_train_id)
     # Fit model
-    clf = cov_neural_net()
+    clf = cov_neural_net(X_train_train, Y_train_train)
+
     clf.fit(X_train_train, Y_train_train,
                batch_size=32, nb_epoch=10, verbose=1,
                validation_data=(X_train_test, Y_train_test))
@@ -245,3 +270,22 @@ if __name__ == '__main__':
 
     print("Predictive quality of the model..")
     print(classification_report(Y_train_test_cat, Yp_train_test_cat))
+
+    if folder_input == 'submission':
+        X_test, X_test_id, __ = etl_images(image_loc=test_dir, subset=100,
+                                                  resize_to=(128, 128), train=False)
+
+        Y_test_prop = clf.predict(X_test, batch_size=32, verbose=2)
+        Y_test_cat = to_class(Y_test_prop)
+
+        prediction = pd.DataFrame({'name': X_test_id,
+                                   'invasive': Y_test_prop[:,1]})
+
+        prediction.set_index('name', drop=True, inplace=True)
+        prediction['invasive'] = round(prediction['invasive'], 3)
+        prediction.sort_index(inplace=True)
+
+        prep_date = time.strftime("%y%m%d_%H%M")
+        filename = 'submission_{}.csv'.format(prep_date)
+
+        prediction.to_csv(filename, sep=',', header=True, index=True)
